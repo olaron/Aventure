4bc07c02bb538599b5efbf76b01e1cf4ded51c7c
rob1fo <alonzo.robin7@gmail.com>
2015-03-03 22:56:29 +0100

    Un peu de rangement

--- js/main.js
+++ js/main.js
@@ -1,199 +1,76 @@
+// Générateurs de formulaires //
+
 function newButton(id, type, text, icon,clickCallback){
-    var icon = $("<span>")
+    var glyph = $("<span>")
         .attr("class", "glyphicon glyphicon-"+icon)
         .attr("aria-hidden", "true");
-    var button = $('<button>')
+    return $('<button>')
         .attr("class", "btn btn-default")
         .attr("type", type)
         .attr("id", id)
         .click(clickCallback)
-        .append(icon , " "+text);
-    return button;
+        .append(glyph , " "+text);
 }
 
-function newInput(id, type, name, placeholder){
-    var input = $("<input>")
+function newInput(name, type, placeholder){
+    return $("<input>")
         .attr("class", "form-control")
         .attr("type", type)
         .attr("name",name)
-        .attr("id", id)
         .attr("placeholder", placeholder);
-    return input;
 }
 
 function newTextarea(id, placeholder, value){
-    var input = $("<textarea>")
+    return $("<textarea>")
         .attr("class", "form-control")
         .attr("id", id)
         .attr("placeholder", placeholder)
         .val(value);
-    return input;
 }
 
 function newHiddenInput(name,value){
-    var input = $("<input>")
+    return $("<input>")
         .attr("type","hidden")
         .attr("name",name)
         .attr("value",value);
-    return input;
 }
 
 function newInputGroup(inputs){
-    var group = $("<div>")
+    return $("<div>")
         .attr("class","input-group")
         .append(inputs);
-    return group;
 }
 
 function newButtonGroup(buttons){
-    var group = $("<span>")
+    return $("<span>")
         .attr("class","input-group-btn")
         .append(buttons);
-    return group;
 }
 
-function newFormulaire(id,inputs,submitCallback){
+function newFormulaire(id,action,classe ,inputs,submitCallback){
     $("#"+id).remove();
-    var form = $("<form>")
+    return $("<form>")
         .attr("id",id)
-        .attr("class","toHide")
+        .attr("class",classe)
         .submit(submitCallback)
-        .append(inputs,newHiddenInput("id",id));
-    return form;
+        .append(inputs,newHiddenInput("action",action));
 }
+
+////////////////////
+
+
+////////// Edition //////////
 
 function creerFormEdition(content){
-    return newFormulaire("form-ecrire",
+    return newFormulaire("form-ecrire","ecriture","toHide",
         [
             newTextarea("text-ecrire","Écrivez ici",content),
             newButton("bouton-valider-ecrire","submit","Valider","ok"),
-            newButton("bouton-annuler","button","Annuler", "remove",DisplayNode)
+            newButton("bouton-annuler","button","Annuler", "remove",DisplayNode),
+            newHiddenInput("id",id)
         ],
         RequestNewNode
     );
-}
-
-function creerFormLien(){
-    return newFormulaire("form-choice",
-        [
-            newInputGroup(
-                [
-                    newInput("","text","choix","Choix"),
-                    $("<span>").attr("class","input-group-addon"),
-                    newInput("","text","destination", "ID de destination"),
-                    newButtonGroup(
-                        [
-                            newButton("bouton-valider-choix","submit","Valider", "ok"),
-                            newButton("bouton-annuler-choix","button","Annuler","remove")
-                        ]
-                    )
-                ]
-            )
-        ],
-        RequestNewLink
-    );
-}
-
-function getURLVariable(variable){
-    var vars = location.search.substring(1).split("&");
-    for (var i=0; i<vars.length; i++){
-        var pair = vars[i].split("=");
-        if(pair[0] == variable){
-            return pair[1];
-        }
-    }
-    return false;
-}
-
-function getId(){
-    id = getURLVariable("id");
-    if (!id) {
-        id = "start";
-        history.replaceState(null,null,location.href+"?id="+id);
-    }
-    RequestNode(id);
-}
-
-function ajaxError(resultat, statut, erreur){
-    console.log(resultat);
-    console.log(statut);
-    console.log(erreur);
-}
-
-function requestSession(){
-    $.ajax({
-        type: "POST",
-        url: "ajax.php",
-        data: {action: "session"},
-        success: onSessionSuccess,
-        error: ajaxError
-    });
-}
-
-function onSessionSuccess(data){
-    if(data.connecte){
-        $("#connecte").fadeIn();
-        $("#pseudo").html(data.pseudo);
-    }
-    else{
-        $("#pas-connecte").fadeIn();
-    }
-}
-
-$(document).ready(function(){
-    $(".toHide").hide();
-
-    getId();
-
-    requestSession();
-
-    $("#btn-inscription").click(function(){
-        $(".navbar-hide")
-            .fadeOut()
-            .promise()
-            .done(function(){
-                $("#form-inscription").fadeIn();
-            })
-    });
-
-    $("#btn-connexion").click(function(){
-        $(".navbar-hide")
-            .fadeOut()
-            .promise()
-            .done(function(){
-                $("#form-connexion").fadeIn();
-            })
-    });
-
-    $(".btn-retour").click(function(){
-        $(".navbar-hide")
-            .fadeOut()
-            .promise()
-            .done(function(){
-                $("#pas-connecte").fadeIn();
-            })
-    });
-
-    $("#bouton-ecrire,#bouton-modifier").click(function(){
-        HideAll(function(){
-            var form = creerFormEdition(node.content);
-            $("#main").prepend(form);
-            form.fadeIn();
-        })
-    });
-
-    $("#bouton-ajouter-choix").click(function(){
-        var form = creerFormLien();
-        $("#main-choices").prepend(form);
-        form.fadeIn();
-
-    });
-
-});
-
-function RequestNewLink(){
-
-    return false;
 }
 
 function RequestNewNode(){
@@ -222,6 +99,181 @@ function OnNewNodeSuccess(data){
     }
 }
 
+////////////////////
+
+
+////////// Lien //////////
+
+function creerFormLien(){
+    return newFormulaire("form-choice","lien","toHide",
+        [
+            newInputGroup(
+                [
+                    newInput("choix","text","Choix"),
+                    $("<span>").attr("class","input-group-addon"),
+                    newInput("destination", "text", "ID de destination"),
+                    newButtonGroup(
+                        [
+                            newButton("bouton-valider-choix","submit","Valider", "ok"),
+                            newButton("bouton-annuler-choix","button","Annuler","remove")
+                        ]
+                    )
+                ]
+            ),
+            newHiddenInput("id",id)
+        ],
+        RequestNewLink
+    );
+}
+
+function RequestNewLink(){
+
+    return false;
+}
+
+////////////////////
+
+
+////////// Connexion //////////
+
+function creerFormConnexion(){
+    return newFormulaire("form-connexion","connexion","navbar-form navbar-right navbar-hide",
+        [
+            newInput("pseudo","text","Pseudo"),
+            newInput("password","password","Mot de passe"),
+            newButton("","submit","Se connecter","off"),
+            newButton("","button","Retour","remove",retourNavbar)
+        ],
+        requestConnexion
+    );
+}
+
+function retourNavbar(){
+    hideNavForms(function(){
+        $("#pas-connecte").fadeIn();
+    });
+}
+
+function requestConnexion(){
+    $.ajax({
+        type: "POST",
+        url: "ajax.php",
+        data: $(this).serialize(),
+        success: onConnexion,
+        error: ajaxError
+    });
+}
+
+function onConnexion(data){
+    alert("todo");
+}
+
+/////////////////////
+
+
+// Fonctions utilitaires //
+
+function getURLVariable(variable){
+    var vars = location.search.substring(1).split("&");
+    for (var i=0; i<vars.length; i++){
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){
+            return pair[1];
+        }
+    }
+    return false;
+}
+
+function getId(){
+    id = getURLVariable("id");
+    if (!id) {
+        id = "start";
+        history.replaceState(null,null,location.href+"?id="+id);
+    }
+    RequestNode(id);
+}
+
+function ajaxError(resultat, statut, erreur){
+    console.log(resultat);
+    console.log(statut);
+    console.log(erreur);
+}
+
+//////////////////////
+
+
+//// Document ready ////
+
+$(document).ready(function(){
+    $(".toHide").hide();
+
+    getId();
+
+    requestSession();
+
+    $("#btn-inscription").click(function(){
+        $(".navbar-hide")
+            .fadeOut()
+            .promise()
+            .done(function(){
+                $("#form-inscription").fadeIn();
+            })
+    });
+
+    $("#btn-connexion").click(function(){
+        hideNavForms(function(){
+            var form = creerFormConnexion();
+            $("#navbar").prepend(form);
+            form.fadeIn();
+        })
+    });
+
+    $(".btn-retour").click(function(){
+        retourNavbar();
+    });
+
+    $("#bouton-ecrire,#bouton-modifier").click(function(){
+        HideAll(function(){
+            var form = creerFormEdition(node.content);
+            $("#main").prepend(form);
+            form.fadeIn();
+        })
+    });
+
+    $("#bouton-ajouter-choix").click(function(){
+        var form = creerFormLien();
+        $("#main-choices").prepend(form);
+        form.fadeIn();
+
+    });
+
+});
+
+/////////////////////
+
+
+// Requêtes de démarrage //
+
+function requestSession(){
+    $.ajax({
+        type: "POST",
+        url: "ajax.php",
+        data: {action: "session"},
+        success: onSessionSuccess,
+        error: ajaxError
+    });
+}
+
+function onSessionSuccess(data){
+    if(data.connecte){
+        $("#connecte").fadeIn();
+        $("#pseudo").html(data.pseudo);
+    }
+    else{
+        $("#pas-connecte").fadeIn();
+    }
+}
+
 function RequestNode(){
     $.ajax({
         type: "POST",
@@ -242,6 +294,11 @@ function OnRequestNodeSuccess(node){
     }
 
 }
+
+////////////////////
+
+
+// Affichage //
 
 function DisplayNode(){
     HideAll(function(){
@@ -266,8 +323,18 @@ function DisplayNode(){
     });
 }
 
+function hideNavForms(callback){
+    $(".navbar-hide")
+        .fadeOut()
+        .promise()
+        .done(callback);
+}
+
 function HideAll(callback){
     $(".toHide")
         .fadeOut()
-        .promise().done(callback);
+        .promise()
+        .done(callback);
 }
+
+////////////////////
